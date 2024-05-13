81511c7e6f49721eaeccd01b56910c107fe39b4e
Salchikiller <caralepenafiel@gmail.com>
2024-05-13 20:16:22 +0200

    init commit

---
+++ src/main/java/util/Graph.java
@@ -0,0 +1,85 @@
+package pr2;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.stream.Collectors;
+
+public class Graph<V>{
+
+    //Lista de adyacencia.
+    private Map<V, Set<V>> adjacencyList = new HashMap<>();
+
+    /**
+     * Añade el vértice `v` al grafo.
+     *
+     * @param v vértice a añadir.
+     * @return `true` si no estaba anteriormente y `false` en caso contrario.
+     */
+    public boolean addVertex(V v){
+	adjacencyList.put(v, new HashSet<V>());
+        return true; //Este código hay que modificarlo.
+    }
+
+    /**
+     * Añade un arco entre los vértices `v1` y `v2` al grafo. En caso de
+     * que no exista alguno de los vértices, lo añade también.
+     *
+     * @param v1 el origen del arco.
+     * @param v2 el destino del arco.
+     * @return `true` si no existía el arco y `false` en caso contrario.
+     */
+    public boolean addEdge(V v1, V v2){
+	adjacencyList.put(v1, new HashSet<V>());
+	adjacencyList.put(v2, new HashSet<V>());
+	adjacencyList.get(v1).add(v2);
+        return true; //Este código hay que modificarlo.
+    }
+
+    /**
+     * Obtiene el conjunto de vértices adyacentes a `v`.
+     *
+     * @param v vértice del que se obtienen los adyacentes.
+     * @return conjunto de vértices adyacentes.
+     */
+    public Set<V> obtainAdjacents(V v) throws Exception{
+	return adjacencyList.get(v);
+    }
+
+    /**
+     * Comprueba si el grafo contiene el vértice dado. 
+     *
+     * @param v vértice para el que se realiza la comprobación.
+     * @return `true` si `v` es un vértice del grafo.
+     */
+    public boolean containsVertex(V v){
+        return adjacencyList.containsKey(v); //Este código hay que modificarlo.
+    }
+
+    /**
+     * Método `toString()` reescrito para la clase `Grafo.java`.
+     * @return una cadena de caracteres con la lista de adyacencia.
+     */
+    @Override
+    public String toString(){
+        return ""; //Este código hay que modificarlo.
+    }
+
+    /**
+     * Obtiene, en caso de que exista, el camino más corto entre
+     * `v1` y `v2`. En caso contrario, devuelve `null`.
+     * 
+     * @param v1 el vértice origen.
+     * @param v2 el vértice destino.
+     * @return lista con la secuencia de vértices del camino más corto
+     * entre `v1` y `v2`
+     **/
+    public List<V> shortestPath(V v1, V v2){
+        return null; // Esto código hay que modificarlo.
+    }
+}
